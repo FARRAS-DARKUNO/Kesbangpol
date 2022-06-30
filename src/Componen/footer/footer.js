f8e507a5b1f2edf0274de248e74476dca2324123
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-06-30 15:12:12 +0700

    benerin footer

--- src/Componen/footer/footer.js
+++ src/Componen/footer/footer.js
@@ -4,6 +4,9 @@ import {
 } from 'react-bootstrap';
 import './footer.css';
 import axios from "axios";
+import { MdEmail, MdLocalPhone, MdFacebook } from 'react-icons/md'
+import { FaYoutube } from 'react-icons/fa'
+import { RiInstagramFill } from 'react-icons/ri'
 
 
 const Footer = () => {
@@ -13,7 +16,7 @@ const Footer = () => {
     const [FooterData, setFooterData] = useState([]);
     useEffect(() => {
         axios
-            .get("http://adminmesuji.embuncode.com/api/instansi/detail/31")
+            .get("http://adminmesuji.embuncode.com/api/instansi/detail/20")
             .then(function (footer) {
                 setFooterData(footer.data.data);
                 console.log("console header: " + footer.data.data);
@@ -28,17 +31,51 @@ const Footer = () => {
             <div className="sub-footer">
                 <div className="aboutUs">
                     <p className="title">Tentang Kami</p>
-                    <p className="text">"Melayani dengan hati"</p>
+                    <p className="text-footer">{FooterData.tentang}</p>
 
                 </div>
                 <div className="contact">
-                    <p className="title">Kontak</p>
-                    <p className="title1">Email</p>
-                    <p className="text">{FooterData.email}</p>
-                    <p className="title1">Nomor Telepon</p>
-                    <p className="text">{FooterData.nomor_telepon}</p>
-                    <p className="title1">Alamat</p>
-                    <p className="text">{FooterData.alamat}</p>
+                    <p className="title">Kontak Kami</p>
+                    <div className="media-sosial-kami">
+                        <div>
+                            <MdEmail size={24} color={'#ffff'} />
+                        </div>
+                        <div className="text-aja-footer">
+                            {FooterData.email}
+                        </div>
+                    </div>
+                    <div className="media-sosial-kami">
+                        <div>
+                            <MdLocalPhone size={24} color={'#ffff'} />
+                        </div>
+                        <div className="text-aja-footer">
+                            {FooterData.nomor_telepon}
+                        </div>
+                    </div>
+                    <div className="media-sosial-kami">
+                        <div>
+                            <MdFacebook size={24} color={'#ffff'} />
+                        </div>
+                        <div className="text-aja-footer">
+                            {FooterData.facebook}
+                        </div>
+                    </div>
+                    <div className="media-sosial-kami">
+                        <div>
+                            <FaYoutube size={24} color={'#ffff'} />
+                        </div>
+                        <div className="text-aja-footer">
+                            {FooterData.youtube}
+                        </div>
+                    </div>
+                    <div className="media-sosial-kami">
+                        <div>
+                            <RiInstagramFill size={24} color={'#ffff'} />
+                        </div>
+                        <div className="text-aja-footer">
+                            {FooterData.instagram}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
