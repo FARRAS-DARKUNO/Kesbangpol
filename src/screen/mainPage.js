fa02be4fd71da6b62ac89cb2748a57e8cdbd1fab
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-06-30 16:54:36 +0700

    fic main page

--- src/screen/mainPage.js
+++ src/screen/mainPage.js
@@ -3,6 +3,7 @@ import { Image } from '@chakra-ui/react'
 import { Carousel } from "react-bootstrap";
 import axios from "axios";
 import { BiChevronsRight } from 'react-icons/bi'
+import { FaAngleRight } from 'react-icons/fa'
 
 import '../style/mainPage.css'
 
@@ -15,12 +16,34 @@ const MainPage = () => {
 
     const [index, setIndex] = useState(0);
     const [newsCard, getNewsCard] = useState(null)
-    const [tentang, getTentang] = useState(null)
-    const [logo, getLogo] = useState(null)
+    const [detailData, getDetailData] = useState(null)
+    const [contain, getContain] = useState(null)
 
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
+
+
+
+    const setContain = async () => {
+        await axios.get("http://adminmesuji.embuncode.com/api/dokumen?instansi_id=15")
+            .then(function (response) {
+                var temp = []
+                for (let i = 0; i < 3; i += 1) {
+                    if (i < response.data.data.data.length) {
+                        temp.push(response.data.data.data[i])
+                    }
+
+                }
+                getContain(temp)
+                console.log('masuk aja 15')
+                console.log(response.data.data.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    }
 
     const setNews = async () => {
         await axios.get("http://adminmesuji.embuncode.com/api/news?instansi_id=2")
@@ -45,10 +68,8 @@ const MainPage = () => {
         await axios.get("http://adminmesuji.embuncode.com/api/instansi/detail/20")
             .then(function (response) {
                 console.log('tentang')
-                console.log(response.data.data.tentang)
-                getTentang(response.data.data.tentang)
-                getLogo(response.data.data.logo_instansi)
-                // console.log(response.data.data.data[0]);
+
+                getDetailData(response.data.data)
             })
             .catch(function (error) {
                 // handle error
@@ -59,7 +80,7 @@ const MainPage = () => {
     useEffect(() => {
         setNews()
         setDetail()
-
+        setContain()
     }, []);
 
 
@@ -84,55 +105,81 @@ const MainPage = () => {
                 )) : <p>Loading</p>}
             </Carousel>
 
-            <div className="article">
-                <div className="sub-article">
-                    <div className="title-real-in-main-page">
-                        <article className="title-article">
-                            Artikel Terbaru
-                        </article>
-                    </div>
+            <div className="layout-aparat-putih">
+                {
+                    detailData != null ?
+                        <div className="article">
+                            <div className="sub-article">
+                                <div className="title-real-in-main-page">
+                                    <article className="title-article">
+                                        {detailData.singkatan_instansi}
+                                    </article>
+                                </div>
+                            </div>
+                            <div className="layout-documen-and-tentang">
+                                <div className="detail-main-layout">
+                                    <Image src={detailData.logo_instansi} className='logo-instansi' />
+                                    <div className="tentang-detail-main">
+                                        <article className="title-detail-mainan">{detailData.nama_instansi}</article>
+                                        <article className="text-detail-mainan"> {detailData.tentang}</article>
+                                    </div>
+                                </div>
+                                <div className="document-main-layout">
+                                    <div className="border-recomendarion-another">
+                                        <div className="tittle-recomend-another">
+                                            Dokumen Terbaru
+                                        </div>
+                                        {
+                                            contain != null ? contain.map((index) => (
+                                                <div className="box-recomend-another">
+                                                    <article className="text-recomend-another">
+                                                        {index.name}
+                                                    </article>
+                                                    <di>
+                                                        <FaAngleRight color="rgb(33, 93, 121)" />
+                                                    </di>
 
-                    <div className="see-more-botton">
-                        <div className="icon-div-center">
-                            <BiChevronsRight color="#ffff" size={24} />
+                                                </div>
+                                            )) : <p>Loading</p>
+                                        }
+
+
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <article className="see-more">
-                            Lihat Lengkap
-                        </article>
-                    </div>
-
-                </div>
-
-                <CardArticleReal />
-
+                        : <p>LOADING</p>
+                }
             </div>
-
-            {/* <div className="article">
-                <div className="sub-tentang">
-                    <article className="title-article">
-                        Tentang
-                    </article>
-                </div>
-
-                <div className="aricle-in">
-                    <div className="flex3">
-                        {
-                            tentang != null ? <article className="tentang-text">{tentang}</article> : <p>loading</p>
-                        }
-                    </div>
-                    <div className="flex1">
-                        {logo != null ? <Image src={logo}
-                            className="header-logo"
-                        /> : <p>Loading</p>}
-                    </div>
-                </div>
-
-            </div> */}
             <div className="layout-aparat">
                 <div className="article">
                     <div className="sub-article">
                         <div className="title-real-in-main-page">
-                            <article className="title-article-struktur">
+                            <article className="title-article">
+                                Artikel Terbaru
+                            </article>
+                        </div>
+
+                        <div className="see-more-botton">
+                            <div className="icon-div-center">
+                                <BiChevronsRight color="#ffff" size={24} />
+                            </div>
+                            <article className="see-more">
+                                Lihat Lengkap
+                            </article>
+                        </div>
+
+                    </div>
+
+                    <CardArticleReal />
+
+                </div>
+            </div>
+            <div className="layout-aparat-putih">
+                <div className="article">
+                    <div className="sub-article">
+                        <div className="title-real-in-main-page">
+                            <article className="title-article">
                                 Struktur
                             </article>
                         </div>
@@ -143,26 +190,27 @@ const MainPage = () => {
                 </div>
             </div>
 
-
-            <div className="article">
-                <div className="sub-article">
-                    <div className="title-real-in-main-page">
-                        <article className="title-article">
-                            Berita Terbaru
-                        </article>
-                    </div>
-                    <div className="see-more-botton">
-                        <div className="icon-div-center">
-                            <BiChevronsRight color="#ffff" size={24} />
+            <div className="layout-aparat">
+                <div className="article">
+                    <div className="sub-article">
+                        <div className="title-real-in-main-page">
+                            <article className="title-article">
+                                Berita Terbaru
+                            </article>
                         </div>
-                        <article className="see-more">
-                            Lihat Lengkap
-                        </article>
+                        <div className="see-more-botton">
+                            <div className="icon-div-center">
+                                <BiChevronsRight color="#ffff" size={24} />
+                            </div>
+                            <article className="see-more">
+                                Lihat Lengkap
+                            </article>
+                        </div>
                     </div>
+
+                    <CardArticle />
+
                 </div>
-
-                <CardArticle />
-
             </div>
         </div>
 
